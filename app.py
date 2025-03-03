302b9734606cc209754b4a49606579113ced330f
Sagar Kamble <151813232+sagarkamble7@users.noreply.github.com>
2025-03-03 10:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 
 # model = pickle.load(open("diabetes_svc_model.pkl", "rb"))
 
-with open(r'D:\Sagar_Kamble_DiabetesPrediction\diabetes_svc_model.pkl', "rb") as f:
+with open(r'diabetes_svc_model.pkl', "rb") as f:
         model = pickle.load(f)
 
 st.title("Diabetes Prediction App")
